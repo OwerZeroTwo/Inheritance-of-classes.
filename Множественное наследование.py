f102fd7e214cc13acff9ea8ac6ae3df4277eef8b
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-04-10 20:37:37 +0300

    Update Множественное наследование.py

--- Множественное наследование.py
+++ Множественное наследование.py
@@ -3,16 +3,15 @@ class Vehicle:
         self.vehicle_type = "none"
 
 
-class Car(Vehicle):
+class Car():
     def __init__(self):
-        super().__init__()
         self.price = 1000000
 
     def horse_powers(self):
         return 200
 
 
-class Nissan(Car):
+class Nissan(Car, Vehicle):
     def __init__(self):
         super().__init__()
         self.vehicle_type = "Внедорожник"
